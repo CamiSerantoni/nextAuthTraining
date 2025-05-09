71008ad9228fe75f643cf9f37664049083d5ca73
CamiSerantoni <camila.serantonir@gmail.com>
2025-05-08 22:51:59 -0400

    adding second part to handling errors

--- components/auth-form.js
+++ components/auth-form.js
@@ -7,7 +7,7 @@ export default function AuthForm() {
   const [formState, formAction] = useFormState(signup, {});
 
   return (
-    <form id="auth-form">
+    <form id="auth-form" action={formAction}>
       <div>
         <img src="/images/auth-icon.jpg" alt="A lock icon" />
       </div>
@@ -19,6 +19,14 @@ export default function AuthForm() {
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
       </p>
+      {formState.errors && (
+        <ul id="form-errors">
+          {Object.keys(formState.errors).map((error) => (
+            <li key={error}>{formState.errors[error]}</li>
+          ))}
+        </ul>
+      )}
+
       <p>
         <button type="submit">Create Account</button>
       </p>
